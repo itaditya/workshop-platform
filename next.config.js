ae318df6d0b5f2172f1d5f300b6c8382b9c27cb4
Aditya Agarwal <adityaa803@gmail.com>
2020-11-21 23:49:24 +0530

    not-working: integrate mermaid

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const rehypePrism = require('@mapbox/rehype-prism');
+var mermaid = require('remark-mermaid');
 const mdx = require('@next/mdx');
 
 const withMDX = mdx({
   options: {
+    remarkPlugins: [mermaid],
     rehypePlugins: [rehypePrism],
   },
 });
